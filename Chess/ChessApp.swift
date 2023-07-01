fc349fb01ece549be3a793fd85d3e4c88f7ac60a
luah5 <raymondvleeshouwer@gmail.com>
2023-07-01 07:32:20 +0100

    Initial Commit

---
+++ Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by Raymond Vleeshouwer on 01/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chess/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Chess
+//
+//  Created by Raymond Vleeshouwer on 01/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
